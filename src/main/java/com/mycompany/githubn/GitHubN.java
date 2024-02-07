30e9c4b1555eaefd0141d626147bc9603173f1e0
Florin <Florin@Ninosishaky>
2024-02-07 20:54:45 +0000

    primer comit 1.0 version

---
+++ src/main/java/com/mycompany/githubn/GitHubN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubn;
+
+/**
+ *
+ * @author Florin
+ */
+public class GitHubN {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
