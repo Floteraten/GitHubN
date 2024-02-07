e8f188e9f2533d4e8183604772ec466f37e5c968
Florin <Florin@Ninosishaky>
2024-02-07 21:57:52 +0000

    primer comit 1.2 version, nuevo archivo, hola carlos

---
+++ src/GithubN/HolaCarlos.java
@@ -0,0 +1,17 @@
+
+package GithubN;
+
+/**
+ *
+ * @author Florin
+ */
+public class HolaCarlos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola carlos");
+    }
+    
+}
